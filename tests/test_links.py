ce19e7ebecd702673891bd905675dd0b715f3c64
Richard <richardadebowale.oye@gmail.com>
2026-03-05 00:41:19 -0500

    added 4 test fucntions:
    test_create_link
    test_load_link_success
    test_load_link_not_found
    test_create_duplicate_link

--- tests/test_links.py
+++ tests/test_links.py
@@ -6,3 +6,18 @@ async def test_create_link(client):
     response = await client.post("/link/", data={"original_url": "https://example.com"})
     assert response.status_code == 200
     assert "http://test/" in response.text
+
+
+@pytest.mark.asyncio
+async def test_load_link_success(client):
+    pass
+
+
+@pytest.mark.asyncio
+async def test_load_link_not_found(client):
+    pass
+
+
+@pytest.mark.asyncio
+async def create_duplicate_link(client):
+    pass
