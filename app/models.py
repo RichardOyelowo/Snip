2d5193a144d4c48b6174bcc3952e42f756bc29c7
Richard <richardadebowale.oye@gmail.com>
2026-03-05 02:50:53 -0500

    fix: shortcode now optional

--- app/models.py
+++ app/models.py
@@ -10,7 +10,7 @@ class Link(Base):
 
     id: Mapped[int] = mapped_column("id", Integer, primary_key=True)
     original_url: Mapped[str] = mapped_column("original_url")
-    short_code: Mapped[str] = mapped_column("short_code", unique=True)
+    short_code: Mapped[Optional[str]] = mapped_column("short_code", unique=True)
     click_count: Mapped[Optional[int]] = mapped_column("click_count",Integer, default=0, nullable=False)
     created_at: Mapped[datetime] = mapped_column("created_at",DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
     
